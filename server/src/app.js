69da26c932d4c9f36516f03480644da546145acb
Anup Khismatrao <khismatrao2003@gmail.com>
2024-12-05 13:48:11 +0530

    init

---
+++ server/src/app.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/v1/authRoutes');
+
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+
+// Routes
+app.use('/api/v1/auth', authRoutes);
+
+module.exports = app;
---
+++ server/src/server.js
@@ -0,0 +1,5 @@
+const app = require('./app');
+
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
