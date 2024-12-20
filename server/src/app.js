db22e5c5cd11115d4468bcb323d930bcdfa63b97
Anup Khismatrao <khismatrao2003@gmail.com>
2024-12-20 21:27:09 +0530

    Fix: Minor import

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const axios = require('axios');
 const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
 const cors = require("cors");
