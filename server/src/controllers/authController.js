9c81b7e6ace746f540820f9cd6c3bd8848fa6e4b
Anup Khismatrao <khismatrao2003@gmail.com>
2025-01-20 15:10:18 +0530

    Update: Commented docusign envelope due to token error

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -14,106 +14,106 @@ const signup = async (req, res) => {
     const userObject = { _id: user._id, name: user.name, email: user.email };
 
     // token for docusign
-    const accessToken = await getAccessToken();
+    // const accessToken = await getAccessToken();
 
-    // DocuSign API client setup
-    const dsApiClient = new docusign.ApiClient();
-    dsApiClient.setBasePath("https://demo.docusign.net/restapi");
-    dsApiClient.addDefaultHeader("Authorization", `Bearer ${accessToken}`);
+    // // DocuSign API client setup
+    // const dsApiClient = new docusign.ApiClient();
+    // dsApiClient.setBasePath("https://demo.docusign.net/restapi");
+    // dsApiClient.addDefaultHeader("Authorization", `Bearer ${accessToken}`);
 
-    const envelopeApi = new docusign.EnvelopesApi(dsApiClient);
+    // const envelopeApi = new docusign.EnvelopesApi(dsApiClient);
 
-    // Load the PDF document
-    const pdfPath = path.resolve( __dirname, "../documents/Consent Form2.pdf"); // Update to the correct path of your PDF
-    const pdfBytes = fs.readFileSync(pdfPath);
-    const pdfBase64 = pdfBytes.toString("base64");
+    // // Load the PDF document
+    // const pdfPath = path.resolve( __dirname, "../documents/Consent Form2.pdf"); // Update to the correct path of your PDF
+    // const pdfBytes = fs.readFileSync(pdfPath);
+    // const pdfBase64 = pdfBytes.toString("base64");
 
-    // Document configuration
-    const document = new docusign.Document();
-    document.documentBase64 = pdfBase64;
-    document.name = "Consent Form for Telehealth Consultation";
-    document.fileExtension = "pdf";
-    document.documentId = "1";
+    // // Document configuration
+    // const document = new docusign.Document();
+    // document.documentBase64 = pdfBase64;
+    // document.name = "Consent Form for Telehealth Consultation";
+    // document.fileExtension = "pdf";
+    // document.documentId = "1";
 
-    // Signer configuration
-    const signer = new docusign.Signer();
-    signer.email = email;
-    signer.name = name;
-    signer.recipientId = "1";
-    signer.routingOrder = "1";
+    // // Signer configuration
+    // const signer = new docusign.Signer();
+    // signer.email = email;
+    // signer.name = name;
+    // signer.recipientId = "1";
+    // signer.routingOrder = "1";
 
-    // Signature tab
-    const signHere = new docusign.SignHere.constructFromObject({
-      anchorString: "Patient Signature:",
-      anchorXOffset: "1.5",
-      anchorUnits: "inches",
-    });
+    // // Signature tab
+    // const signHere = new docusign.SignHere.constructFromObject({
+    //   anchorString: "Patient Signature:",
+    //   anchorXOffset: "1.5",
+    //   anchorUnits: "inches",
+    // });
 
-    // Name tab
-    const nameTab = new docusign.Text.constructFromObject({
-      anchorString: "Name:",
-      anchorXOffset: "1",
-      anchorUnits: "inches",
-      fontSize: "size12",
-      tabLabel: "Name",
-      value: name,
-    });
+    // // Name tab
+    // const nameTab = new docusign.Text.constructFromObject({
+    //   anchorString: "Name:",
+    //   anchorXOffset: "1",
+    //   anchorUnits: "inches",
+    //   fontSize: "size12",
+    //   tabLabel: "Name",
+    //   value: name,
+    // });
 
-    // Email tab
-    const emailTab = new docusign.Text.constructFromObject({
-      anchorString: "Email Address:",
-      anchorXOffset: "1",
-      anchorUnits: "inches",
-      fontSize: "size12",
-      tabLabel: "Email",
-      value: email, 
-    });
+    // // Email tab
+    // const emailTab = new docusign.Text.constructFromObject({
+    //   anchorString: "Email Address:",
+    //   anchorXOffset: "1",
+    //   anchorUnits: "inches",
+    //   fontSize: "size12",
+    //   tabLabel: "Email",
+    //   value: email, 
+    // });
 
-    // Date tab
-    const dateTab = new docusign.DateSigned.constructFromObject({
-      anchorString: "Date:",
-      anchorXOffset: "1",
-      anchorUnits: "inches",
-      tabLabel: "DateSigned",
-    });
-
-    
-    const refTab = new docusign.Text.constructFromObject({
-      anchorString: "Referred By: ",
-      anchorXOffset: "50",
-      anchorUnits: "pixels",
-      fontSize: "size12",
-      tabLabel: "ReferredBy",
-      value: referral || "",
-      required: false
-    });
-
-
-    // Combine all tabs
-    const tabs = new docusign.Tabs.constructFromObject({
-      signHereTabs: [signHere],
-      textTabs: [nameTab, emailTab, refTab],
-      dateSignedTabs: [dateTab],
-    });
-    signer.tabs = tabs;
-
-    // Envelope definition
-    const envelopeDefinition = new docusign.EnvelopeDefinition();
-    envelopeDefinition.emailSubject = "Please sign this agreement document";
-    envelopeDefinition.documents = [document];
-    envelopeDefinition.recipients = {
-      signers: [signer],
-    };
-    envelopeDefinition.status = "sent";
-
-    // console.log("Envelope Definition:", JSON.stringify(envelopeDefinition, null, 2));
+    // // Date tab
+    // const dateTab = new docusign.DateSigned.constructFromObject({
+    //   anchorString: "Date:",
+    //   anchorXOffset: "1",
+    //   anchorUnits: "inches",
+    //   tabLabel: "DateSigned",
+    // });
 
     
-    // Send the envelope
-    const envelopeResult = await envelopeApi.createEnvelope(process.env.DS_ACCOUNT_ID, { envelopeDefinition });
+    // const refTab = new docusign.Text.constructFromObject({
+    //   anchorString: "Referred By: ",
+    //   anchorXOffset: "50",
+    //   anchorUnits: "pixels",
+    //   fontSize: "size12",
+    //   tabLabel: "ReferredBy",
+    //   value: referral || "",
+    //   required: false
+    // });
 
-    // Save the envelope ID to the user
-    user.docusignEnvelopeId = envelopeResult.envelopeId;
+
+    // // Combine all tabs
+    // const tabs = new docusign.Tabs.constructFromObject({
+    //   signHereTabs: [signHere],
+    //   textTabs: [nameTab, emailTab, refTab],
+    //   dateSignedTabs: [dateTab],
+    // });
+    // signer.tabs = tabs;
+
+    // // Envelope definition
+    // const envelopeDefinition = new docusign.EnvelopeDefinition();
+    // envelopeDefinition.emailSubject = "Please sign this agreement document";
+    // envelopeDefinition.documents = [document];
+    // envelopeDefinition.recipients = {
+    //   signers: [signer],
+    // };
+    // envelopeDefinition.status = "sent";
+
+    // // console.log("Envelope Definition:", JSON.stringify(envelopeDefinition, null, 2));
+
+    
+    // // Send the envelope
+    // const envelopeResult = await envelopeApi.createEnvelope(process.env.DS_ACCOUNT_ID, { envelopeDefinition });
+
+    // // Save the envelope ID to the user
+    // user.docusignEnvelopeId = envelopeResult.envelopeId;
     await user.save();
 
     res.status(201).json({
